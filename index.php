3b8ddcd1a3bf07201ea3d41d906bb955da2dc027
allsupport <141170000+allsupport@users.noreply.github.com>
2025-03-11 02:42:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
-});
 
-async function handleRequest(request) {
-  let country = request.headers.get("CF-IPCountry");
-
-  if (country === "US") {
-    return Response.redirect("https://huntop001.z13.web.core.windows.net/", 301);
-  } else if (country === "CA") {
-    return Response.redirect("https://huntop001.z13.web.core.windows.net/", 301);
-  } else {
-    return Response.redirect("https://www.allrecipes.com/", 301);
-  }
-}
