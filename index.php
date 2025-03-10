ea809b3100311474105e5199e4b1c1e77ba5d6ff
allsupport <141170000+allsupport@users.noreply.github.com>
2025-03-11 00:21:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<div class="modalContent">
-            <div id="dismissModal" class="closeBtn">Close</div>
             <h1>Cookie Policy</h1>
             <p>This website uses cookies to personalize content and ads, provide social media features, and analyze our traffic. By clicking Accept, you agree to the use of cookies. For more information, please visit our <a href="https://oyster-app-4ewv7.ondigitalocean.app/">Privacy Policy</a>.</p>
             <button id="confirmBtn" class="actionBtn">Accept</button>
